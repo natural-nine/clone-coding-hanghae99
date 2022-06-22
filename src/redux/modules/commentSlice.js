44a077158f2945ac40963536599ca55f36a0bca3
yebin76 <djaak9604@naver.com>
2022-06-22 09:57:50 +0900

    FEAT:댓글 불러오기 기능구현, 댓글 뷰 디테일 살림

--- src/redux/modules/commentSlice.js
+++ src/redux/modules/commentSlice.js
@@ -1,16 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios';
 
-const initialState = { value: 0 }
 
-const counterSlice = createSlice({
-  name: 'comments',
+const initialState = {comments:[]}
+
+//미들웨어
+//post id값 넣어주기
+export const loadCommentAX = () =>{
+  return function(dispatch){
+    //http://54.180.114.134
+      // axios.get(`http://54.180.121.151/api/comment/${post_id}`)
+      axios.get(` http://localhost:5001/comments`)
+      .then(response =>dispatch(loadComments(response.data)))
+  }
+}
+
+const commentSlice = createSlice({
+  name: 'comment',
   initialState,
   reducers: {
-    getCommnets(state,action) {
- 
+    loadComments(state,action) {
+      state.comments = action.payload
     }
   },
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
-export default counterSlice.reducer
+export const {loadComments} = commentSlice.actions
+export default commentSlice.reducer
